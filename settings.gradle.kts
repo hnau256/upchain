2c56e39aaae3dbc39c218f6188ff2f37d0ddcc59
Mark Zorikhin <hnau256@gmail.com>
2026-04-01 08:31:18 +0300

    Update settings plugin and project version to 1.2.0
    - Update `org.hnau.plugin.settings` to version 1.9.0.
    - Increment project publication version to 1.2.0.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,14 @@ pluginManagement {
 }
 
 plugins {
-    id("org.hnau.plugin.settings") version "1.8.2"
+    id("org.hnau.plugin.settings") version "1.9.0"
 }
 
 hnau {
     groupId = "org.hnau.upchain"
 
     publish {
-        version = "1.1.1"
+        version = "1.2.0"
         gitUrl = "https://github.com/hnau256/upchain"
     }
 }
